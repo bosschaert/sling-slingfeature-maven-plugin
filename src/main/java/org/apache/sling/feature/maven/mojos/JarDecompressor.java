b91764023240c4c5766b9d408b64ca478b13e9cf
David Bosschaert <bosschae@adobe.com>
2019-09-16 15:08:43 +0200

    WIP

--- src/main/java/org/apache/sling/feature/maven/mojos/JarDecompressor.java
+++ src/main/java/org/apache/sling/feature/maven/mojos/JarDecompressor.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.sling.feature.maven.mojos;
 
 import java.io.BufferedOutputStream;
@@ -90,20 +106,21 @@ class JarDecompressor {
 
     private static class SizeAndCrcComputeStream extends OutputStream {
         final CRC32 crc = new CRC32();
-        long runningSize = 0;
+        long sizeCounter = 0;
 
         public void write(int r) throws IOException {
-            runningSize++;
+            sizeCounter++;
             crc.update(r);
         }
 
         public void write(byte[] b, int off, int len) throws IOException {
-            runningSize += len;
+            sizeCounter += len;
             crc.update(b, off, len);
         }
 
         public void updateZipEntry(ZipEntry e) {
-            e.setSize(runningSize);
+            e.setSize(sizeCounter);
+            e.setCompressedSize(sizeCounter);
             e.setCrc(crc.getValue());
             e.setMethod(ZipEntry.STORED);
         }
--- src/test/java/org/apache/sling/feature/maven/mojos/JarDecompressorTest.java
+++ src/test/java/org/apache/sling/feature/maven/mojos/JarDecompressorTest.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.sling.feature.maven.mojos;
 
 import org.junit.Test;
@@ -12,6 +28,7 @@ import java.util.Map;
 import java.util.jar.JarEntry;
 import java.util.jar.JarInputStream;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -31,10 +48,25 @@ public class JarDecompressorTest {
         }
     }
 
+    @Test
+    public void testSignedJar() {
+
+    }
+
+    @Test
+    public void testJarWithEmbeddedJar() {
+
+    }
+
     private void assertJarsEqual(File cj, File uj) throws IOException {
-        Map<String, byte[]> expectedBytes = readJar(cj);
-        Map<String, byte[]> actualBytes = readJar(uj);
-        assertEquals(expectedBytes, actualBytes);
+        Map<String, byte[]> expectedJar = readJar(cj);
+        Map<String, byte[]> actualJar = readJar(uj);
+
+        assertEquals(expectedJar.size(), actualJar.size());
+        for (Map.Entry<String, byte[]> entry : expectedJar.entrySet()) {
+            byte[] actual = actualJar.get(entry.getKey());
+            assertArrayEquals(entry.getValue(), actual);
+        }
     }
 
     private Map<String, byte[]> readJar(File jar) throws IOException {
